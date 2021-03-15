0a14161b6ba0b197b8e47be83b570d71aa2fdc59
Maciej Poręba <macpor9@gmail.com>
2021-03-16 00:26:31 +0100

    initial commit

---
+++ backend/src/main/java/pl/maciejp/fphoto/FphotoApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejp.fphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FphotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FphotoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/maciejp/fphoto/FphotoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejp.fphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FphotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
